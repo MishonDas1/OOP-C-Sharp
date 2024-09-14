4968d20f5857d8f6b4cc82eb4dbcb75b016743f7
Mishon24 <mishon.da@uttarauniversity.edu.bd>
2024-09-15 01:54:53 +0600

    first commit

---
+++ OOP Second/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+    class Person 
+{
+    public string name;
+    public int age;
+
+    public void Method()
+    {
+       Console.WriteLine($"First Person Name:{name} And Age is {age}"); 
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+     Person p1 = new Person();
+        p1.name = "Shiahab";
+        p1.age = 22;
+        p1.Method();
+        
+
+        Person p2 =new Person();
+        p2.name = "Mishon";
+        p2.age = 22;
+       p2.Method();
+        
+    }
+}
