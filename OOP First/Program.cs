09a103ce300fd45f8678384e30adfa535126a051
Mishon24 <mishon.da@uttarauniversity.edu.bd>
2024-09-15 01:59:06 +0600

    New

---
+++ OOP First/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Person 
+{
+    public string name;
+    public int age;
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+     Person p1 = new Person();
+        p1.name = "Shiahab";
+        p1.age = 22;
+        Console.WriteLine($"First Person Name:{p1.name} And Age is {p1.age}");
+
+
+
+
+        Person p2 =new Person();
+        p2.name = "Mishon";
+        p2.age = 22;
+        Console.WriteLine($"First Person Name:{p2.name} And Age is {p2.age}");
+        
+    }
+}
