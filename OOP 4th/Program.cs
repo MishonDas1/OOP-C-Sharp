685ecc72ae2971fd997db3147b4678acb404d8ed
Mishon24 <mishon.da@uttarauniversity.edu.bd>
2024-09-17 19:02:06 +0600

    4th OOP Constructor
    Constructor

---
+++ OOP 4th/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+    class Person 
+{
+    public string name;
+    public int age;
+
+    public Person(){
+      name ="Mishon Das";
+      age =20;
+
+    }
+    public Person(string n,int a){
+      name =n;
+      age =a;
+
+    }
+    
+
+    public void Method()
+    {
+       Console.WriteLine($" Person Name:{name} And Age is {age}"); 
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+     Person p1 = new Person("Arnab kumar" ,21);
+       
+        p1.Method();
+        
+
+        Person p2 =new Person("Jahed Hasan",25);
+        p2.Method();
+        
+        Person p3 =new Person();
+        p3.Method();
+    }
+}
