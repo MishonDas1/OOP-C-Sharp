43176e658250804fcda709c133aafd5043c07c77
Mishon24 <mishon.da@uttarauniversity.edu.bd>
2024-09-23 21:26:16 +0600

    One to 5 Code
    OOP 1 to 5 code only

---
+++ 1 to 5 only code/4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyNamespace
+{
+
+  class Person
+  {
+    
+    public string name;
+    public int age;
+
+    public Person(string n,int a){
+      name =n;
+      age =a;
+    }
+
+    public Person(){
+      name ="Mishon Das";
+      age = 23;
+
+    }
+
+    public void Method(){
+         Console.WriteLine($"{name}   {age}");
+      
+    }
+  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1=new Person("Mishon kumar Das",55);
+            p1.Method();
+
+            Person p2 =new Person();
+            p2.Method();
+           
+        }
+    }
+}
---
+++ 1 to 5 only code/5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+    class Person 
+{
+    public string name;
+    private int age;
+
+    public void setage(int a){
+        age =a;
+    }
+
+    public int getage(){
+        return age;
+    }
+
+    
+
+    
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+     Person p1 = new Person();
+     p1.name=("Mishon Das");
+     p1.setage(33);
+       
+    Console.WriteLine($" Person Name:{p1.name} And Age is {p1.getage()}"); 
+    }
+}
