4d95bb4f979d397bd89c9c82ed0379500acc4b8a
Mishon24 <mishon.da@uttarauniversity.edu.bd>
2024-09-24 21:24:01 +0600

    for each
    important

---
+++ for each/Program.cs
@@ -0,0 +1,22 @@
+﻿using System ;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main ( string [] args )
+        {
+            int [] numbers ={1,2,3,4,5};
+            int sum = 0;
+
+            foreach(int number in numbers){
+                sum=sum+number;
+            }
+            Console.WriteLine(sum);
+
+            /*for(int i=0;i<names.Length;i++){
+                Console.WriteLine(names[i]);
+            }*/
+        }
+    }
+}
+
